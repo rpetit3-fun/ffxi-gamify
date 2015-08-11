dc5ae9450c5059b9c5ae48aae82a7f9d5840e226
Robert A Petit III <robbie.petit@gmail.com>
2015-08-11 17:24:04 -0400

    add configuration files for nginx + supervisor + gunicorn setup

---
+++ config/gunicorn-conf.py
@@ -0,0 +1,13 @@
+bind = "127.0.0.1:8000"
+
+workers = 8
+worker_class = 'sync'
+worker_connections = 1000
+timeout = 30
+keepalive = 2
+
+loglevel = 'info'
+errorlog = '/home/rpetit/ffxi-gamify/logs/gunicorn_error.log'
+accesslog = '/home/rpetit/ffxi-gamify/logs/gunicorn_access.log'
+
+preload = True
